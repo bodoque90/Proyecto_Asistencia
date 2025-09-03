eb1817326033c7641b8e3a0844dff00ed22ee041
maflita69 <z25logc@gmail.com>
2025-09-03 18:01:12 -0400

    visual menu juna

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 import express from 'express';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
+
+
+const __filename = fileURLToPath(import.meta.url);
+
+app.get('/menu', (req, res) => {
+    res.sendFile(path.join(path.dirname(__filename), 'views', 'index.html'));
+});
